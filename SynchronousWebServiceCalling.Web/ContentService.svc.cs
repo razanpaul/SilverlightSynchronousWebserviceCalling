a135f59bcf1f1c33166dc1bfce5274597405ef0f
rp <rpaul@outlook.com>
2012-10-06 16:01:09 +1000

    initial code

---
+++ SynchronousWebServiceCalling.Web/ContentService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.IO;
+using System.Collections;
+
+namespace SynchronousWebServiceCalling.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ContentService
+    {      
+        [OperationContract]
+        public string ReturnServerTime()
+        {
+            return DateTime.Now.ToString();
+        }
+    }   
+}
---
+++ SynchronousWebServiceCalling/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace SynchronousWebServiceCalling
+{
+    public partial class MainPage : UserControl
+    {
+        private  AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        public MainPage()
+        {
+            InitializeComponent();
+            StartServerTimeUpdatingSyncronosly();
+        }
+        private void StartServerTimeUpdatingSyncronosly()
+        {
+            try
+            {
+                Thread thread = new Thread(new ThreadStart(UpdateTimeSyncronosly));
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "SynchronousWebServiceCalling", MessageBoxButton.OK);
+            }
+        }     
+        private void UpdateTimeSyncronosly()
+        {
+            SynchronousWebServiceCalling.ContentServiceReference.ContentServiceClient contentServiceClient = new SynchronousWebServiceCalling.ContentServiceReference.ContentServiceClient();
+            contentServiceClient.ReturnServerTimeCompleted += new EventHandler<SynchronousWebServiceCalling.ContentServiceReference.ReturnServerTimeCompletedEventArgs>(ReturnServerTimeCompleted);
+            string message = "Retrieving new server time...";
+            while (true)
+            {
+                this.Dispatcher.BeginInvoke(new Action<string>(DisplayBusyMessage), message);
+                contentServiceClient.ReturnServerTimeAsync();
+                autoResetEvent.WaitOne();
+            }
+        }
+        void ReturnServerTimeCompleted(object sender, SynchronousWebServiceCalling.ContentServiceReference.ReturnServerTimeCompletedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action<string>(DisplayTime), e.Result);         
+            string message = "New server time is retrieved";
+            this.Dispatcher.BeginInvoke(new Action<string>(DisplayBusyMessage), message);
+            System.Threading.Thread.Sleep(5000);
+            autoResetEvent.Set();
+        }
+        private void DisplayTime(string time)
+        {
+            tblTime.Text = time;
+        }
+        private void DisplayBusyMessage(string message)
+        {
+            StatusLabel.Text = message;
+        }
+    }
+}
